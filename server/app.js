381693880d7c2af8323586be2dec4f07de561560
Ben Margis <margis.ben@gmail.com>
2016-08-30 16:28:17 -0500

    Redeploy

--- server/app.js
+++ server/app.js
@@ -3,8 +3,28 @@ var bodyParser = require('body-parser');
 var app = express();
 var index = require('./routes/index');
 var path = require('path');
+var mongodb = require("mongodb");
+
+var ObjectID = mongodb.ObjectID;
+var CONTACTS_COLLECTION = "contacts";
+
+var db;
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+});
+
+// Save database object from the callback for reuse.
+db = database;
+console.log("Database connection ready");
+
+
 
 var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
